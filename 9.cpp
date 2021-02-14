dd37e14ecab849102efbbe18bca3a8a3572ae2b3
wonjr <41326736+wonjr@users.noreply.github.com>
2021-02-14 23:13:23 +0900

    모두의 약수

---
+++ 9.cpp
@@ -0,0 +1,43 @@
+#include<cstdio>
+/*
+//1
+int main()
+{
+	int N, i, j;
+	int cnt[50001] = { 0 };
+	scanf("%d", &N);
+	for (i = 1; i <= N; i++)
+	{
+		for (j = i; j <= N; j += i)
+		{
+			cnt[j]++;
+		}
+	}
+	for (i = 1; i <= N; i++)
+	{
+		printf("%d ", cnt[i]);
+	}
+	return 0;
+}
+*/
+/*
+//2
+int main()
+{
+	int N, i, j, cnt;
+
+	scanf("%d", &N);
+
+	for (i = 1; i <= N; i++)
+	{
+		cnt = 0;
+		for (j = 1; j <= i; j++)
+		{
+			if (i % j == 0)
+				cnt++;
+		}
+		printf("%d ",cnt);
+	}
+}
+*/
+
