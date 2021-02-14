04041bbf30fa721bd67b692fe9ff02762e7760a3
wonjr <41326736+wonjr@users.noreply.github.com>
2021-02-14 22:55:51 +0900

    1부터 N까지 M의 배수합

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int N, M, hap = 0, i;
+	cin >> N >> M;
+	for (i = 1; i <= N; i++)
+		if (i % M == 0)
+			hap += i;
+	cout << hap;
+	return 0;
+}
