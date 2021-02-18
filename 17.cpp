c2117e2e88d10ccb64f99ae4f1324679ca935f15
wonjr <41326736+wonjr@users.noreply.github.com>
2021-02-18 22:43:10 +0900

    선생님 퀴즈

---
+++ 17.cpp
@@ -0,0 +1,23 @@
+#include<cstdio>
+
+int main()
+{
+	int n, sum = 0, i, j, m, ans;
+
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++)
+	{
+		scanf("%d %d", &m, &ans);
+		sum = 0;
+		for (j = 1; j <= m; j++)
+		{
+			sum += j;
+		}
+		if (ans == sum)
+			printf("YES\n");
+		else
+			printf("NO\n");
+	}
+	return 0;
+}
